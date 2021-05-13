b0a7c818cf53c606f12b6e07a08c1f21014e0257
sebastian <slarrauri@gmail.com>
2021-05-13 16:17:57 -0300

    feat(core): add initApp function

--- bin/changelog_creator_sample.dart
+++ bin/changelog_creator_sample.dart
@@ -1,5 +1,6 @@
 import 'package:changelog_creator_sample/changelog_creator_sample.dart' as changelog_creator_sample;
 
 void main() {
-
+  print('////////// CHANGELOG CREATOR //////////');
+  print(changelog_creator_sample.initApp());
 }
