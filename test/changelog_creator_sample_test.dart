d14c5efe53b8fa24fbb7a1919b9568cd6d18343a
sebastian <slarrauri@gmail.com>
2021-05-13 16:14:22 -0300

    remove(test): remove test

--- test/changelog_creator_sample_test.dart
+++ test/changelog_creator_sample_test.dart
@@ -1,8 +1,1 @@
-import 'package:changelog_creator_sample/changelog_creator_sample.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
