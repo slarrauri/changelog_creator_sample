3df2bbddd8609b2ebf7886c294160cc448ed500c
sebastian <slarrauri@gmail.com>
2021-05-13 15:59:25 -0300

    init(core): Initial commit

---
+++ test/changelog_creator_sample_test.dart
@@ -0,0 +1,8 @@
+import 'package:changelog_creator_sample/changelog_creator_sample.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
