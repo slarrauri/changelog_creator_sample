80564ba6712d37e26a5ca27f9b15cf0c93789a8c
sebastian <slarrauri@gmail.com>
2021-05-13 16:02:03 -0300

    remove(core): remove boilerplate code

--- bin/changelog_creator_sample.dart
+++ bin/changelog_creator_sample.dart
@@ -1,5 +1,5 @@
 import 'package:changelog_creator_sample/changelog_creator_sample.dart' as changelog_creator_sample;
 
-void main(List<String> arguments) {
-  print('Hello world: ${changelog_creator_sample.calculate()}!');
+void main() {
+
 }
